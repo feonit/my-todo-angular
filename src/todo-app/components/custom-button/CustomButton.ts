946ab2385d6207bab90c6299dbaf7a838b9399bf
feonit <feonitu@yandex.ru>
2017-04-04 11:40:34 +0300

    'update'

--- src/todo-app/components/custom-button/CustomButton.ts
+++ src/todo-app/components/custom-button/CustomButton.ts
@@ -1,4 +1,3 @@
-/*import IScope = angular.IScope;*/
 import { Component } from '../../models/Component';
 
 export interface ICustomButton {
@@ -7,44 +6,30 @@ export interface ICustomButton {
   disabled: boolean;
 }
 
-const _defaults = function(target: any, src: any) {
-  Object.keys(src).forEach(function(key: string) {
-    target[key] = typeof target[key] === 'undefined' ? src[key] : target[key];
-  });
-  return target;
-};
-
-class CustomButtonComponentController {
-  options:  ICustomButton;
+class CustomButtonController {
   class:  string;
   title:  string;
   disabled: boolean;
 
-  constructor(){
-
-  }
-
-  $onInit(){
-
-  }
-
   $onChanges(props){
-    this.title = props.options.currentValue.title;
-    this.class = props.options.currentValue.class;
-    this.disabled = props.options.currentValue.disabled;
+    this.title = props.title.currentValue;
+    this.class = props.class.currentValue;
+    this.disabled = props.disabled.currentValue;
   }
 }
 
-export default new class CustomButtonComponent extends Component{
+class CustomButton extends Component{
   constructor() {
     super({
       templateUrl: 'todo-app/components/custom-button/CustomButton.html',
-      controller: CustomButtonComponentController,
+      controller: CustomButtonController,
       bindings: {
-        options: '<',
-        // class: '<',
-        // title: '<'
+        class: '<',
+        disabled: '<',
+        title: '<'
       }
     });
   }
 }
+
+export default new CustomButton
